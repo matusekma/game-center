9c7dba4d6e8996ed6d708e1b2eb15a8e4c945f65
Marton Matusek <matusekma@gmail.com>
2018-09-08 16:31:52 +0200

    Port number changed for Heroku

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.use(express.static('public'));
 app.use(express.static('views'));
 app.use(express.static('games'));
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 5000, function(){
 
 });
